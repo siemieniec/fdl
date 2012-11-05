477c8175869fd80268cf7cc7331a09cc3ec77665
Robert Siemieniec <robert@newvue.com>
2012-11-05 14:52:08 -0500

    Add _ clear_logs_ button

--- lib/fdl.rb
+++ lib/fdl.rb
@@ -5,42 +5,48 @@ require "haml"
 require "date"
 
 module Fdl
-    class WebApp < Sinatra::Base
-        set :root, File.expand_path("../..", __FILE__)
+  class WebApp < Sinatra::Base
+    set :root, File.expand_path("../..", __FILE__)
 
-        get "/" do
-            [200, { "context-type" => "text/html"}, 
-                File.read("public/index.html")]
-        end
-
-        get "/log" do
-            if params[:message].nil? || 
-                /\A\s*\Z/ =~ params[:message]
-                halt 400
-            end
-
-            WebApp.push(params[:message])
-            204
-        end
-
-        get "/show" do
-            @date = Date.today
-            if File.exists?(WebApp.log_file_path)
-                @logs = File.readlines(WebApp.log_file_path).reverse
-            else 
-                @logs = ["Nothing logged yet"]
-            end
-            haml :show
-        end
-
-        def self.push message
-            File.open(log_file_path, "a+") do |f|
-                f.puts "<small>#{Time.now.strftime("%m/%d/%Y at %H:%M")} </small> #{message}"
-            end 
-        end
-
-        def self.log_file_path
-            File.expand_path(File.dirname(__FILE__)).gsub(/\/lib\z/, "/public/log.txt")
-        end
+    get "/" do
+      [200, { "context-type" => "text/html"},
+        File.read("public/index.html")]
     end
+
+    get "/log" do
+      if params[:message].nil? || /\A\s*\Z/ =~ params[:message]
+        halt 400
+      end
+
+      WebApp.push(params[:message])
+      204
+    end
+
+    get "/show" do
+      @date = Date.today
+      if File.exists?(WebApp.log_file_path)
+        @logs = File.readlines(WebApp.log_file_path).reverse
+      else
+        @logs = ["Nothing logged yet"]
+      end
+      haml :show
+    end
+
+    get "/clear" do
+      if File.exists?(WebApp.log_file_path)
+        File.delete(WebApp.log_file_path)
+      end
+      redirect "/show"
+    end
+
+    def self.push message
+      File.open(log_file_path, "a+") do |f|
+        f.puts "<small>#{Time.now.strftime("%m/%d/%Y at %H:%M")} </small> #{message}"
+      end
+    end
+
+    def self.log_file_path
+      File.expand_path(File.dirname(__FILE__)).gsub(/\/lib\z/, "/public/log.txt")
+    end
+  end
 end
